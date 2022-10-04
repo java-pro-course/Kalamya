d904925a86817d9bc762f96602cfe52d2a690281
Kalamya <markvor2077@gmail.com>
2022-10-05 00:42:58 +0300

    First task

---
+++ 1_2_MarkVoronchihin.java
@@ -0,0 +1,12 @@
+
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+public static void main(String[] args){
+	Scanne in = new Scanner(System.in);
+	int a = in.nextInt();
+
+   System.out.println(Math.pow(a,3));
+	}
+ }
